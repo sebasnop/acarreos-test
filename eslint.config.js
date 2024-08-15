c1869afc894cbc8b48e225f7dd92895f3d0bd4f4
sebasnop <sevalenciazapata@gmail.com>
2024-08-14 20:09:04 -0500

    First commit

---
+++ eslint.config.js
@@ -0,0 +1,51 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import react from 'eslint-plugin-react'
+import reactPlugin from 'eslint-plugin-react'
+
+
+export default tseslint.config({
+  extends: [
+    js.configs.recommended, 
+    ...tseslint.configs.recommendedTypeChecked,
+    ...tseslint.configs.stylisticTypeChecked,
+  ],
+  files: ['**/*.{ts,tsx}'],
+  ...reactPlugin.configs.flat.recommended,
+  ignores: ['dist'],
+  languageOptions: {
+    ecmaVersion: 2020,
+    globals: globals.browser,
+    parserOptions: {
+      project: ['./tsconfig.node.json', './tsconfig.app.json'],
+      tsconfigRootDir: import.meta.dirname,
+      ecmaFeatures: {
+        jsx: true
+      }
+    },
+    globals: {
+      ...globals.browser,
+      ...globals.serviceworker
+    }
+  },
+  settings: { react: { version: '18.3' } },
+  plugins: {
+    'react-hooks': reactHooks,
+    'react-refresh': reactRefresh,
+    react
+  },
+  rules: {
+    ...reactHooks.configs.recommended.rules,
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+    ...react.configs.recommended.rules,
+    ...react.configs['jsx-runtime'].rules
+  },
+})
