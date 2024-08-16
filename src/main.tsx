a1d3b027d1ae5fbc317abf5aed513a112996f48d
sebasnop <sevalenciazapata@gmail.com>
2024-08-15 23:02:17 -0500

    feat: Adding React Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { RouterProvider, RouterProviderProps } from "react-router-dom";
+import { router } from "./router/router.tsx"
 import './index.css'
+
+const routerProps: RouterProviderProps['router'] = router;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={routerProps}/>
   </React.StrictMode>,
 )
