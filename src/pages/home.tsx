c1a68e86b0ad29e41cfbbb2559fcc0bfb2302108
sebasnop <sevalenciazapata@gmail.com>
2024-08-16 22:04:01 -0500

    feat: Example header created for examining a Tailwind template.

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,12 @@
-import { HeaderCustom } from '../components/header'
+//import { HeaderCustom } from '../components/header'
+import ExampleHeader from '../components/example-header'
 
 export function HomePage() {
 
   return (
     <>
-      <HeaderCustom/>
+      <ExampleHeader/>
+      {/*<HeaderCustom/>*/}
       <section className='py-3 md:py-8 max-w-4xl mx-auto'>
         <h1 className="text-3xl font-bold">
           ¡Acarreos Appa!
