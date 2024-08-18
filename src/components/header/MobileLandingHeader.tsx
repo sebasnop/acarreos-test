b23ad04feaa63a7e31d50689c4e9d46982cb1856
sebasnop <sevalenciazapata@gmail.com>
2024-08-18 01:54:57 -0500

    fix: Closing dialog mobile menu when user is redirected to track shipment section
    It was not closing the menu when the user was already at home page.

--- src/components/header/MobileLandingHeader.tsx
+++ src/components/header/MobileLandingHeader.tsx
@@ -46,6 +46,7 @@ export default function MobileLandingHeader({ setMobileMenuOpen, mobileMenuOpen,
 
               <Link
                 to={routes.track.href}
+                onClick={() => setMobileMenuOpen(false)}
                 className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
               >
                 {routes.track.name}
