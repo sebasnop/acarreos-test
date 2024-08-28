cb6e5bd9141ab90cee5e00f6e444266d554b81e8
sebasnop <sevalenciazapata@gmail.com>
2024-08-28 01:55:29 -0500

    fix: Minor color adjustment.

--- src/components/track/TrackShipmentForm.tsx
+++ src/components/track/TrackShipmentForm.tsx
@@ -60,7 +60,7 @@ export default function TrackShipmentForm() {
               type="number"
               className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-950 shadow-sm
                 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400
-                focus:ring-2 focus:ring-inset focus:ring-yellow-950 sm:text-sm sm:leading-6"
+                focus:ring-2 focus:ring-inset focus:ring-yellow-900 sm:text-sm sm:leading-6"
             />
           </div>
         </div>
