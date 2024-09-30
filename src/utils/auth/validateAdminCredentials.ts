e83b69f835464548d02ce6f8cb5353f5a7e86a40
sebasnop <sevalenciazapata@gmail.com>
2024-09-29 20:19:13 -0500

    refactor: Change Enterprise to Admin credentials

---
+++ src/utils/auth/validateAdminCredentials.ts
@@ -0,0 +1,13 @@
+import { AdminsTable } from "@/database/AdminsTable";
+import { AdminInterface } from "@/interfaces/DatabaseInterfaces";
+
+/**
+ * Valida las credenciales de una empresa.
+ * 
+ * @param username - El nombre de usuario de la empresa.
+ * @param password - La contraseña de la empresa.
+ * @returns La empresa si las credenciales son correctas, o null si no lo son.
+ */
+export default function validateAdminCredentials(username: string, password: string): AdminInterface | null {
+  return AdminsTable.find(admin => admin.username === username && admin.password === password) || null;
+}
